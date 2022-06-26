d602d1147627498738c54f404bc88406fd616b88
unknown <kawsarahmed4148@gmail.com>
2022-06-26 13:21:26 +0600

    undefined vs null cleared

--- undefined-vs-null.js
+++ undefined-vs-null.js
@@ -1,3 +1,13 @@
 // Undefined is  variable that has not been assigned a value
+var person; // Value is undefined, type is undefined
+console.log(typeof person);
 
 // In JavaScript, null is a special value that represents an empty or unknown value
+var person = null; // Value is null, but type is still an object
+console.log(typeof person);
+
+// difference between null vs undefined
+typeof undefined // undefined
+typeof null // object
+null === undefined // false
+null == undefined // true
